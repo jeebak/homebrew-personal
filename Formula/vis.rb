bcd1079ff0ec2c4a3cf16fb8599627c646b1ec13
JeeBak Kim <jeebak.kim@gmail.com>
2016-04-03 23:17:08 -0700

    Add desc and home to vis Formula

--- Formula/vis.rb
+++ Formula/vis.rb
@@ -1,6 +1,6 @@
 class Vis < Formula
-  desc ""
-  homepage ""
+  desc "a vim like text editor"
+  homepage "https://github.com/martanne/vis"
   url "https://github.com/martanne/vis/archive/v0.2.tar.gz"
   version "0.2"
   sha256 "3e5b81d760849c56ee378421e9ba0f653c641bf78e7594f71d85357be99a752d"
@@ -16,15 +16,6 @@ class Vis < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test vis`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end

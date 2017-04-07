6b094df4fcf73072d8367da8ecafc5268ef4d56f
JeeBak Kim <jeebak.kim@gmail.com>
2017-04-07 03:17:46 -0700

    Update Formula/atto.rb

--- Formula/atto.rb
+++ Formula/atto.rb
@@ -2,9 +2,8 @@ class Atto < Formula
   desc "Atto Emacs is a minimal functional Emacs in less than 2000 lines of C code"
   homepage "https://github.com/hughbarney/atto"
   # No tags, so using this for now
-  url "https://github.com/hughbarney/atto/archive/master.tar.gz"
-  sha256 "e8aa013f9f274acc51da8f383b53c4cb70d873ca6d3e85a14e9a4217021198c9"
-  version "644ba20a"
+  url "https://github.com/hughbarney/atto", :using => GitDownloadStrategy
+  version "current"
 
   # depends_on "cmake" => :build
 

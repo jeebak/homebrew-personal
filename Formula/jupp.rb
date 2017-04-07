92e359e79eaa032b7da4b3e97f7d18410ab0d44e
JeeBak Kim <jeebak.kim@gmail.com>
2017-04-07 03:25:01 -0700

    Update Formula/jupp.rb

--- Formula/jupp.rb
+++ Formula/jupp.rb
@@ -1,9 +1,9 @@
 class Jupp < Formula
   desc "jupp - The Editor which sucks less"
   homepage "https://www.mirbsd.org/jupp.htm"
-  url "https://www.mirbsd.org/MirOS/dist/jupp/joe-3.1jupp28.tgz"
-  version "28"
-  sha256 "be4d16cb44f002a3f6f5069471096c53d9c184b7366ef1a055422d2f0006851f"
+  url "https://www.mirbsd.org/MirOS/dist/jupp/joe-3.1jupp30.tgz"
+  version "3.1jupp28"
+  sha256 "65ddb346364a056c1d78a1cb406b0ebf6c9c2fbd753cd404b1b4c8fd3fa9916d"
 
   def install
     system "bash", "./configure", "--prefix=#{prefix}"

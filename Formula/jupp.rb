c7b2c73abb11211f24db7568cc984aadce2fc8dc
JeeBak Kim <jeebak.kim@gmail.com>
2017-04-08 23:03:40 -0700

    Update Formula/jupp.rb

--- Formula/jupp.rb
+++ Formula/jupp.rb
@@ -2,8 +2,10 @@ class Jupp < Formula
   desc "jupp - The Editor which sucks less"
   homepage "https://www.mirbsd.org/jupp.htm"
   url "https://www.mirbsd.org/MirOS/dist/jupp/joe-3.1jupp30.tgz"
-  version "3.1jupp28"
+  version "3.1jupp30"
   sha256 "65ddb346364a056c1d78a1cb406b0ebf6c9c2fbd753cd404b1b4c8fd3fa9916d"
+
+  conflicts_with "joe", :because => "Jupp is the portable version of JOE’s Own Editor"
 
   def install
     system "bash", "./configure", "--prefix=#{prefix}"
